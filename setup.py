cb36083cf5a57d8ac93ae0037f69608aa22ee129
Student ApnaCollege <digambarrasal1234@gmail.com>
2024-02-14 17:17:11 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Digambarnr',
+    author_email= 'digambar1234@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
